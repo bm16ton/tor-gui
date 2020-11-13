61aa5f2db9d36f7faaf667db8800a709ccc12381
bm16ton <bm16ton@gmail.com>
2020-11-12 21:37:14 -0500

    update list-macs.sh for two column outputbetter for awk

--- list-macs.sh
+++ list-macs.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 #ifconfig | grep -i ether | awk '{print $2}'
-ip -o link | grep ether | awk '{ print $2" : "$17 }'
+ip -o link | grep ether | awk '{ print $2" : "$17 }' > /var/lib/tor-gui/macs
+sed -i 's/: :/ /g' /var/lib/tor-gui/macs
+
